b5a70e92efa03658ffa8c42bc3dfae67c8fe2ee0
Dave H <brew7982@outlook.com>
2022-01-21 07:19:13 -0600

    Added time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}. Time is {{today.toLocaleTimeString()}}</div>`,
 })
 export class AppComponent {
   value = 'David Hahn';
+  today = null;
+
+  constructor() {
+    this.today = new Date();
+    //var dd = String(today.getDate()).padStart(2, '0');
+    //var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    //var yyyy = today.getFullYear();
+    //this.tstamp = mm + '/' + dd + '/' + yyyy;
+  }
 }
